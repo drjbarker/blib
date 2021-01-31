00b46d12f5ef1085734cf567286f7b39d739ce95
Joseph Barker <j.barker@leeds.ac.uk>
2021-01-31 22:29:20 +0000

    Fix spliting of LTWA lines and detect ‘eng’ and ‘mul’
    The LTWA file is tab separated but we were splitting on any whitespace causing words with spaces in to be split incorrectly and making it harder to detect the languages.
    
    Now we correctly split the fields by tab and then split the language field by comma and strip the whitespace.
    
    We also now accept ‘mul’ as a language which stands for multiple and contains many english abbreviations.

--- ltwa/ltwa_to_dict.py
+++ ltwa/ltwa_to_dict.py
@@ -27,18 +27,32 @@ LTWA_FILE = "LTWA_20160915.txt"
 
 print('LTWA_ABBREV = {')
 
+# format on each line is
+# fullword or stem | abbrev (or n.a.) | languages (comma separated)
+# There are some times where the fullword is more than a single word
+# e.g. 'Ann Arbor' or sometimes the abbrev contains spaces, e.g. U. K.
+#
+# The file is formatted as a tab separated format and so we can split on
+# tabs to separate the fields and maintain spaces within words
+#
 with io.open(LTWA_FILE, 'r', encoding='utf16') as f:
     for line in f.readlines():
-        data = line.split()
+        data = line.strip().split('\t')
+        # print(data)
         word_pattern = data[0]
         word_replacement = data[1]
 
         if word_replacement == 'n.a.':
             # this word does not get abbreviated in ISO 4
             continue
-        languages = [x.replace(',','') for x in data[2:]]
 
-        if 'eng' in languages:
+        # no language was defined on the line for some reason
+        if not len(data) == 3:
+            continue
+
+        languages = [x.strip() for x in data[2].split(',')]
+
+        if 'eng' in languages or 'mul' in languages:
             regex_word_pattern = word_pattern
             regex_word_pattern = re.sub(r'^-', r'\\S+', regex_word_pattern)
             regex_word_pattern = re.sub(r'-$', r'\\S*', regex_word_pattern)
