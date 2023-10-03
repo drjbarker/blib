1962a4407895f6916a4f0e4170b24b12f89d9e17
Joseph Barker <j.barker@leeds.ac.uk>
2023-10-03 14:32:12 +0100

    Use unidecode to normalise unicode to ascii if available
    This does a better job than the standardised unicode normalisation in cases where an alphabet treats similar 'looking' letters as distinct letters, not letters with diacritics.

--- formatting/bibtex.py
+++ formatting/bibtex.py
@@ -5,11 +5,20 @@ from encoding.latex import LatexEncoder
 import unicodedata
 from collections import OrderedDict
 
+try:
+    from unidecode import unidecode
+    has_unidecode = True
+except:
+    has_unidecode = False
+
 def normalise_unicode_to_ascii(string):
     """
     Removes any non-ascii characters from the string, replacing them with latin equivalents if possible
     """
-    return unicodedata.normalize('NFKD', string).encode('ascii', 'ignore').decode()
+    if has_unidecode:
+        return unidecode(string)
+    else:
+        return unicodedata.normalize('NFKD', string).encode('ascii', 'ignore').decode()
 
 def flatten(x):
     """
